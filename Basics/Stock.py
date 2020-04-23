593e4d1f9cdaa2d9aa4eee2f84839aafa7213092
Milad Bohlouli <miladbohlouli@gmail.com>
2020-04-23 16:21:43 +0430

    Sequence prediction model added

--- Basics/Stock.py
+++ Basics/Stock.py
@@ -64,9 +64,10 @@ window_size = 12
 iterations = 10
 learning_rate = 0.001
 load = True
+test_count = 20
 
 if __name__ == '__main__':
-    fd = flight_dataset(window_size)
+    fd = flight_dataset(window_size, test_count=test_count)
 
     train_loader = DataLoader(fd, batch_size=batch_size)
 
@@ -112,17 +113,16 @@ if __name__ == '__main__':
     my_model.eval()
     test_data, test_labels, normalization = fd.get_test()
     prediction = list(test_data)
-    prediction_length = 12
 
     with torch.no_grad():
-        for _ in range(prediction_length):
+        for _ in range(test_count):
             out = my_model(torch.from_numpy(np.array(prediction[-window_size:])[None, :, None]))
             prediction.append(out.item())
 
-    actual_predictions = (np.array(prediction[-prediction_length:]) * normalization[1]) + normalization[0]
+    actual_predictions = (np.array(prediction[-test_count:]) * normalization[1]) + normalization[0]
 
-    plt.plot(list(range(prediction_length)), actual_predictions)
-    plt.plot(list(range(prediction_length)), test_labels)
+    plt.plot(list(range(test_count)), actual_predictions)
+    plt.plot(list(range(test_count)), test_labels)
     plt.legend(["predicted", "truth"]), plt.grid(alpha=0.2)
     plt.show()
 
