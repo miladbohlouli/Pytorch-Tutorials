62b1a0e01204dd374716e62f4274c87695d43e1b
Milad Bohlouli <miladbohlouli@gmail.com>
2021-02-09 15:11:04 +0330

    some new tests on new dataset added

--- Basics/test.py
+++ Basics/test.py
@@ -1,80 +1,58 @@
-import torch
-import torchvision as tv
-from torchvision import transforms
-from torch.utils import data
-from torch import nn
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
 from matplotlib import pyplot as plt
-import time
-import os
+# import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
+import torch
+from time import time
+from os import listdir
 
-sw = SummaryWriter("./checkpoint/")
+class insurance(Dataset):
+    def __init__(self):
+        dataset = pd.read_csv("../datasets/insurance.csv")
+        dataset = dataset.select_dtypes(exclude=["object"]).to_numpy()
+        self.X, self.Y = dataset[:, :-1], dataset[:, -1]
 
-dataset_train = tv.datasets.MNIST("../datasets",
-                                  train=True,
-                                  transform=transforms.ToTensor(),
-                                  download=True)
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
 
+    def __len__(self):
+        return len(self.X)
 
-dataset_test = tv.datasets.MNIST("../datasets",
-                                  train=False,
-                                  transform=transforms.ToTensor(),
-                                  download=True)
-
-
-train_loader = data.DataLoader(dataset_train,
-                               batch_size=16,
-                               shuffle=True)
-
-
-test_loader = data.DataLoader(dataset_train,
-                               batch_size=16,
-                               shuffle=False)
-
-
-def save_model(dictionary, path="."):
-    torch.save(dictionary, f"{path} + {time.time()}")
-
-def load_model(path="./save"):
-    models = sorted(os.listdir(path))
-    return torch.load(f"{path}/{models[-1]}")
+    def __str__(self):
+        return str(self.X.shape)
 
 
 class FFNN(nn.Module):
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, hidden_layers):
         super(FFNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 1000)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(1000, num_classes)
+        self.layers = []
+        hidden_layers.insert(0, input_size)
+        hidden_layers.append(1)
+        for l0, l1 in zip(hidden_layers[:-1], hidden_layers[1:]):
+            self.layers.append(nn.Linear(l0, l1))
 
-    def forward(self, x):
-        layers1 = self.relu(self.fc1(x))
-        return self.fc2(layers1)
+
+def save(dictionary, path="./save"):
+    torch.save(dictionary, f"{path}/{time()}")
+
+
+def load(path="./"):
+    files = sorted(listdir(path))
+    return torch.load(f"{path}/{files[-1]}")
 
 
 if __name__ == '__main__':
+    dataset = insurance()
+    train_loader = DataLoader(dataset,
+                              batch_size=64)
 
-    mymodel = FFNN(28*28, 10)
-    optimizer = torch.optim.Adam(mymodel.parameters())
-    loss = nn.CrossEntropyLoss()
-    num_epochs = 15
+    my_model = FFNN(input_size=3, hidden_layers=[1000, 1000])
+    crriterian = nn.MSELoss()
+    optim = torch.optim.Adam()
 
-    for epoch in range(100):
-        for i, (x, y) in enumerate(train_loader):
-
-            # Forward path
-            x = x.reshape((-1, 28*28))
-            outputs = mymodel(x)
-            ls = loss(outputs, y)
-
-            # Backward path
-            optimizer.zero_grad()
-            ls.backward()
-            optimizer.step()
-
-        print(f"The loss in epoch {epoch}: {ls:.2}")
-        sw.add_scalar("training_loss", ls)
 
 
 
